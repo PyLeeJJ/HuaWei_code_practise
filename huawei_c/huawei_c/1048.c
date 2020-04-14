9fc54c00b8071c9040850ab3c6d8bdd73e1f3a3e
LeeJJ <lijunjie19943601@sina.com>
2020-04-14 15:25:49 +0800

    //test

---
+++ huawei_c/huawei_c/1048.c
@@ -0,0 +1,14 @@
+//
+//  1048.c
+//  huawei_c
+//
+//  Created by 李俊杰 on 2020/4/14.
+//  Copyright © 2020 李俊杰. All rights reserved.
+//
+
+//最长字符串链
+//输入：["a","b","ba","bca","bda","bdca"]
+//输出：4
+//解释：最长单词链之一为 "a","ba","bda","bdca"
+
+
---
+++ huawei_c/huawei_c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  huawei_c
+//
+//  Created by 李俊杰 on 2020/4/14.
+//  Copyright © 2020 李俊杰. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
